7134b34558cd848ee40183f80d25e12ea71f02ca
ElectricCatHub <80710915+ElectricCatHub@users.noreply.github.com>
2021-06-03 20:57:36 +0700

    Finish code challenge

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+const myname = "Erwin";
+const year = new Date().getFullYear();
+
+ReactDom.render(<div>
+  <p>Created by {myname}</p>
+  <p>&copy; 2020-{year}</p>
+  </div>, 
+  document.getElementById("root"));
